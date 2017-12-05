72c412486a86cc50f1f81dbee6a66f61edca3c4e
Zabrina Larson <zabrinalarson@gmail.com>
2017-12-05 05:02:10 +0000

    Integrated skills via the model generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121035446) do
+ActiveRecord::Schema.define(version: 20171205025001) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
