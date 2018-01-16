1a66a9b525b4e04d9462dfa8234e7b3a6abc902e
Zabrina Larson <zabrinalarson@gmail.com>
2018-01-16 05:53:36 +0000

    Integrated new and create functions on portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,22 @@ class PortfoliosController < ApplicationController
     def index
         @portfolio_items = Portfolio.all
     end
+    
+    def new 
+        @portfolio_items = Portfolio.all
+    end
+    
+    def create
+    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_items.save
+        format.html { redirect_to portfolio_path, notice: 'Portfolio item was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_items }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+      end
+    end
+    end
 end
