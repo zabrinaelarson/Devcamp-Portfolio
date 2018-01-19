37efeaa4235b89afe904c1098b4c7d1f8821c6e8
Zabrina Larson <zabrinalarson@gmail.com>
2018-01-19 04:34:22 +0000

    Integrated edit and update functionalities

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,7 +16,22 @@ class PortfoliosController < ApplicationController
         format.json { render :show, status: :created, location: @portfolio_items }
       else
         format.html { render :new }
-        format.json { render json: @portfolio_items.errors, status: :unprocessable_entity }
+      end
+    end
+    end
+    
+    def edit
+      @portfolio_item = Portfolio.find (params[:id])
+    end
+    
+    def update
+      @portfolio_item = Portfolio.find (params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Record was successfully updated.' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
       end
     end
     end
