72bdd2dd48b6858aa4e0c32eecb274c7fa19eccc
Zabrina Larson <zabrinalarson@gmail.com>
2018-04-03 04:17:54 +0000

    Refractored strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,7 +14,7 @@ class PortfoliosController < ApplicationController
     
     
     def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -32,8 +32,9 @@ class PortfoliosController < ApplicationController
     
     def update
       @portfolio_item = Portfolio.find (params[:id])
+      
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Record was successfully updated.' }
         format.json { render :show, status: :ok, location: @blog }
       else
@@ -57,5 +58,14 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
+    end
+    
+    private
+    
+    def portfolio_params
+      params.require(:portfolio).permit(:title, 
+                                        :subtitle,
+                                        :body,
+                                        technologies_attributes: [:name])
     end
 end
