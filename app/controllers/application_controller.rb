b3ccb8eb1505925b3982504cd87c7ae063e5d673
Zabrina Larson <zabrinalarson@gmail.com>
2018-06-12 03:01:53 +0000

    Refractored the trademarktool

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,6 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
-  before_action :set_copyright
-  
-  def set_copyright
-    @copyright = ZsViewTool::Renderer.copyright 'Zabrina Larson', 'All Rights Reserved'  
-  end
-  
-  module ZsViewTool
-  class Renderer
-    def self.copyright name, msg
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end
-  end
-  end
-  
+  include ZsViewTool
  
 end
