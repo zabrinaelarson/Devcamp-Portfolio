362c61527f1368a1f741e28b22fe4094d2d85c9e
Zabrina Larson <zabrinalarson@gmail.com>
2018-06-12 02:47:21 +0000

    Added trademark as a running bottom

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,19 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+  before_action :set_copyright
   
-   
+  def set_copyright
+    @copyright = ZsViewTool::Renderer.copyright 'Zabrina Larson', 'All Rights Reserved'  
+  end
+  
+  module ZsViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
+  end
+  
+ 
 end
