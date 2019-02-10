a7caf699aa62ce94980b39ddbe28294e5970bcf5
Zabrina Larson <zabrinalarson@gmail.com>
2019-02-10 05:16:34 +0000

    Implemented persistent position updates for drag and drrop interface

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,6 +6,14 @@ class PortfoliosController < ApplicationController
         @portfolio_items = Portfolio.by_position
     end
     
+    def sort
+      params[:order].each do |key, value|
+        Portfolio.find(value[:id]).update(position: value[:position])
+      end
+      
+      head :ok
+    end
+    
     def new 
         @portfolio_item = Portfolio.new
         3.times { @portfolio_item.technologies.build }
