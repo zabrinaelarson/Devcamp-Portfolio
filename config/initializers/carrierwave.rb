e047f5fbbafdb4ff8506e0cfc8e6cfae06796af6
Zabrina Larson <zabrinalarson@gmail.com>
2019-03-19 04:15:07 +0000

    Updated file upload forms and refractored placeholder functionality into view

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,8 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
-  config.aws_bucket = ENV.fetch('S3_BUCKET_NAME') 
-  config.aws_acl    = 'private'
-
+  config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
+  config.aws_acl    = 'public-read'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
-
   config.aws_attributes = {
     expires: 1.week.from_now.httpdate,
     cache_control: 'max-age=604800'
@@ -13,12 +11,6 @@ CarrierWave.configure do |config|
   config.aws_credentials = {
     access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    region:            ENV.fetch('AWS_REGION'), # Required
-    stub_responses:    Rails.env.test?
+    region:            ENV.fetch('AWS_REGION') # Required
   }
-
-  config.paperclip_defaults = { 
-      s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
-   }
-
-end
+end 
