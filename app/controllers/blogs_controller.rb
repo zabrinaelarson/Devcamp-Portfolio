35ad4a6dedccc2f9f980a0e524e4d2c7d3a98cb6
Zabrina Larson <zabrinalarson@gmail.com>
2019-07-12 20:24:08 -0600

    Built out Initial Form and Partial Call for comments along with controller actions

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,7 +13,11 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    @page_title= @blog.title
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+    @comment = Comment.new
+
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
