11bc28757f8e3f0dfb54839577781ccbccd3ee2a
Zabrina Larson <zabrinalarson@gmail.com>
2019-07-27 14:47:53 -0600

    Created a comment breadcast, cofigured the websocket route, and updated the comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :users
   resources :portfolios, except: [:show] do
@@ -15,6 +14,7 @@ Rails.application.routes.draw do
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ActionCable.server=> '/cable'
   root to: 'pages#home'
   
   resources :blogs do
