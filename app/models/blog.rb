19583a8eec9fe67ffae709d0621cc41349b77cd5
Zabrina Larson <zabrinalarson@gmail.com>
2019-08-23 22:28:02 -0600

    Implemented custom scope for recent  blogs and hide draft posts from non admins

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,8 @@ class Blog < ApplicationRecord
     enum status: { draft: 0, published:1 }
     extend FriendlyId
   friendly_id :title, use: :slugged
+
+  default_scope {order(created_at: :desc)}
   
   validates_presence_of :title, :body
   
