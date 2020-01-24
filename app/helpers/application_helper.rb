3d7d3dc4813249128e14d255340d85be4737e35c
Zabrina Larson <zabrinalarson@gmail.com>
2020-01-23 23:04:42 -0700

    Implemented styles for the Portfolio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,9 @@ module ApplicationHelper
          " ".html_safe +
          (link_to "Login", new_user_session_path, class: style) 
         else
-         link_to "Logout", destroy_user_session_path, method: :delete, class: style
+         (link_to "Account", edit_user_registration_path, class: style) +
+         " ".html_safe +
+         (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
         end
     end
     
